197e4f4ee3e925b9816e5c3b274e792c22de88c3
Shrikantpaliwal <shrikantpaliwalgrt@gmail.com>
2021-01-01 02:16:36 +0530

    Stable login/Register API

--- routes/auth.js
+++ routes/auth.js
@@ -1,32 +1,52 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const {RegisterValidation, LoginValidation} = require('../validation/Validation');
+const bcrypt = require('bcryptjs');
 
-const VReq = require('joi');
-
-const ValRule = VReq.object({
-    name: VReq.string().min(6).max(255).required(),
-    email: VReq.string().min(6).max(255).required().email(),
-    password: VReq.string().min(6).max(255).required()
-});
 
 router.post('/register', async (req, res) =>{
-    const {error} = ValRule.validate(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
-    
-    console.log(req.body);
+    // //Validation
+    // const {error} = RegisterValidation(req.body);
+    // if(error) return res.status(400).send(error.details[0].message);
+
+    //User Already Exists
+    const EmailExists = await User.findOne({email : req.body.email});
+    if(EmailExists) return res.status(400).send({"error": "true","Message": "User Already Exists"});
+
+    //Salting Password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    //Add to the DataBase
     const user = User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
      });
     
     try{
         const savedUser = await user.save();
-        res.send(savedUser);
+        res.send(savedUser); // To-Do: Dont send complete User
     }
     catch(err){
         res.send(err);
     }
+});
+
+router.post('/login', async (req, res)=>{
+    //Validation
+    const {error} = LoginValidation(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    //User Exists
+    const UserExists = await User.findOne({email : req.body.email});
+    if(!UserExists) return res.status(400).send({"error": "true","Message": "Issue with Email or Password"});
+
+    //Check Password
+    const ValidPass = await bcrypt.compare(req.body.password, UserExists.password);
+    if(!ValidPass) return res.status(400).send({"error": "true","Message": "Wrong Password"});
+
+    return res.send({"error": "false","Message": "Login Success"});   
 });
 
 
