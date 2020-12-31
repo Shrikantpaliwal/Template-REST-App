e0497a924f0d525d9e95e649938d1d4e19086034
Shrikantpaliwal <shrikantpaliwalgrt@gmail.com>
2020-12-31 22:53:00 +0530

    Validation Added

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,18 @@
 const router = require('express').Router();
 const User = require('../model/User');
 
+const VReq = require('joi');
+
+const ValRule = VReq.object({
+    name: VReq.string().min(6).max(255).required(),
+    email: VReq.string().min(6).max(255).required().email(),
+    password: VReq.string().min(6).max(255).required()
+});
+
 router.post('/register', async (req, res) =>{
+    const {error} = ValRule.validate(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+    
     console.log(req.body);
     const user = User({
         name: req.body.name,
